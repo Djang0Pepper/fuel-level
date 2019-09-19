20a547fad9a25bf56086bdf2e1e2f80ddbbe4370
Djang0Pepper <call2me@free.fr>
2019-09-19 17:05:26 +0200

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Arduino.h> 
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN 16
+#endif
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
